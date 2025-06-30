5e7a8afc31050754244bc72cd06fd968644d7795
eXway <r.okenczyc123@gmail.com>
2025-06-30 17:04:42 +0200

    Test tooltip

--- src/components/Forms/InputLabel.vue
+++ src/components/Forms/InputLabel.vue
@@ -2,7 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { Tooltip } from '../index.js';
+import { Tooltip } from '../../index';
 
 library.add(faCircleQuestion);
 
@@ -15,6 +15,9 @@ defineProps(['value', 'sublabel', 'required', 'customClass', 'tooltip']);
         <span v-else><slot /></span>
         <span v-if="!!sublabel" class="ml-2 text-xs text-gray-500">{{ sublabel }}</span>
         <span class="ml-1 text-red-500" v-if="required">*</span>
+        <!-- Debug: Show tooltip prop value -->
+        <span v-if="tooltip" class="ml-1 text-xs text-blue-500">[TOOLTIP: {{ tooltip }}]</span>
+        <span v-else class="ml-1 text-xs text-red-500">[NO TOOLTIP]</span>
         <Tooltip v-if="tooltip" :content="tooltip" class="ml-1 inline-block">
             <FontAwesomeIcon :icon="faCircleQuestion" class="h-3 w-3 cursor-help text-gray-400 transition-colors hover:text-gray-600" />
         </Tooltip>
