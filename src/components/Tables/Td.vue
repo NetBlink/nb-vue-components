5466bd1175cd06fe725083320a107797b92407f9
eXway <r.okenczyc123@gmail.com>
2025-07-03 07:11:32 +0200

    restyle tables

--- src/components/Tables/Td.vue
+++ src/components/Tables/Td.vue
@@ -5,9 +5,7 @@ interface TdProps {
 const props = defineProps<TdProps>();
 </script>
 <template>
-    <td
-        class="rounded-lg py-2 text-center whitespace-nowrap transition group-hover:bg-red-500 hover:bg-gray-50 has-[>&:hover]:bg-gray-600 md:px-6 md:py-4 md:text-left"
-    >
+    <td class="rounded-lg py-2 text-center whitespace-nowrap transition hover:bg-gray-50 md:px-6 md:py-4 md:text-left">
         <span class="td-label hidden font-bold">{{ props.label }}</span>
         <slot />
     </td>
