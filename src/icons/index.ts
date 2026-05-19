e8bbd4d087e09f5b04864a3d65ae9c99938c7535
eXway <r.okenczyc123@gmail.com>
2026-05-19 16:04:42 +0200

    public re-exports for ./icons subpath

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as NbIcon } from './NbIcon.vue';
+export { createNbIcons } from './createNbIcons';
+export { useNbIcons } from './inject';
+export { defaultAliases } from './aliases';
+export type { AliasName, IconLike, IconSet, NbIconsOptions, RawSvg, SetPrefixed } from './types';
